1015d23398737b6434fe260f8a2ca3701e366091
antocecere <antocecere77@gmail.com>
2020-04-29 12:41:19 +0200

    Array destructuring

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,7 +4,7 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  const inputState = useState({title: '', amoount: ''});
+  const [ inputState, setInputState] = useState({ title: '', amount: ''});
 
   const submitHandler = event => {
     event.preventDefault();
@@ -17,19 +17,26 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={inputState[0].title} 
+            <input type="text" id="title" value={inputState.title} 
                 onChange={event => {
                   const newTitle = event.target.value;
-                  inputState[1](prevInputState => ({title: newTitle, amount: prevInputState.amount}))
+                  setInputState(prevInputState => ({title: newTitle, amount: prevInputState.amount}));
                 }}/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={inputState[0].amount} 
-                onChange={event => {
-                  const newAmount = event.target.value;
-                  inputState[1](prevInputState => ({title: prevInputState.title, amount: newAmount}))
-                }}/>
+            <input
+              type="number"
+              id="amount"
+              value={inputState.amount}
+              onChange={event => {
+                const newAmount = event.target.value;
+                setInputState(prevInputState => ({
+                  amount: newAmount,
+                  title: prevInputState.title
+                }));
+              }}
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
