b6110a92ce2a9dab0c11b0e3e9e1ac3cbe88a0d8
antocecere <antocecere77@gmail.com>
2020-04-30 10:32:10 +0200

    Deleting ingredients

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -33,8 +33,14 @@ const Ingredients = () => {
   };
 
   const removeIngredientHandler = ingredientId => {
-    setUserIngredients(prevIngredients => 
-      prevIngredients.filter(ingredient => ingredient.id !== ingredientId));
+    fetch(`https://react-hooks-update-e3310.firebaseio.com/ingredients/${ingredientId}.json`, 
+      {
+        method: 'DELETE'
+      }
+      ).then(response => {
+        setUserIngredients(prevIngredients => 
+          prevIngredients.filter(ingredient => ingredient.id !== ingredientId));
+      })
   };
 
   return (
