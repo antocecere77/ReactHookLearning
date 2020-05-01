b787986dae1af586913ea7c2c27da11d4771557b
antocecere <antocecere77@gmail.com>
2020-05-01 12:39:24 +0200

    Using the custom hook

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -24,8 +24,8 @@ const ingredientReducer = (currentIngredients, action) => {
 const Ingredients = () => {
 
   const [ userIngredients, dispatch ] = useReducer(ingredientReducer, []);
-  const { isLoading, error, data, sendRequest, reqExtra, reqIdentifier } = useHttp();
-  
+  const { isLoading, error, data, sendRequest, reqExtra, reqIdentifier, clear } = useHttp();
+
   useEffect(() => {
     if(!isLoading && !error && reqIdentifier === 'REMOVE_INGREDIENT') {
       dispatch({type: 'DELETE', id: reqExtra}); 
@@ -52,7 +52,7 @@ const Ingredients = () => {
         JSON.stringify(ingredient),
         ingredient,
         'ADD_INGREDIENT');   
-  }, []);
+  }, [sendRequest]);
 
   const removeIngredientHandler = useCallback(ingredientId => {
     sendRequest(
@@ -64,10 +64,6 @@ const Ingredients = () => {
     );
   }, [sendRequest]);
 
-  const clearError = useCallback(() => {
-    //dispatchHttp({type: 'CLEAR'});
-  }, []);
-
   const ingredientList = useMemo(() => {
     return (
       <IngredientList 
@@ -78,7 +74,7 @@ const Ingredients = () => {
 
   return (
     <div className="App">
-      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
       <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading}/>
 
       <section>
--- src/hooks/http.js
+++ src/hooks/http.js
@@ -1,5 +1,13 @@
 import { useReducer, useCallback } from 'react';
 
+const initialState = {
+  loading: false, 
+  error: null,
+  data: null, 
+  extra: null, 
+  identifier: null
+};
+
 const httpReducer = (curHttpState, action) => {
     switch (action.type) {
       case 'SEND':
@@ -7,22 +15,16 @@ const httpReducer = (curHttpState, action) => {
       case 'RESPONSE':
         return { ...curHttpState, loading: false, data: action.responseData, extra: action.extra};
       case 'ERROR':
-        return {loading: false, error: action.errorData};
-      case 'CLEAR':
-        return { ...curHttpState, error: null };
-     
+        return {loading: false, error: action.errorData};     
+      case 'CLEAR': return initialState;
       default: throw new Error('Should not get there!');
     }
   }
 
 const useHttp = () => {
-    const [ httpState, dispatchHttp ] = useReducer(httpReducer, {
-        loading: false, 
-        error: null,
-        data: null, 
-        extra: null, 
-        identifier: null
-    });
+    const [ httpState, dispatchHttp ] = useReducer(httpReducer, initialState);
+
+    const clear = useCallback(() => { dispatchHttp({type: 'CLEAR'})}, []);
 
     const sendRequest = useCallback((url, method, body, reqExtra, reqIdentifier) => {
       dispatchHttp({type: 'SEND', identifier: reqIdentifier});
@@ -49,7 +51,8 @@ const useHttp = () => {
         error: httpState.error,
         sendRequest: sendRequest,
         reqExtra: httpState.extra,
-        reqIdentifier: httpState.identifier
+        reqIdentifier: httpState.identifier,
+        clear: clear
     };
 };
 
